9bbffa525ec26fb9614167a7c6088eff5d25c5e3
Pablo Tack <tackpablo@gmail.com>
2022-03-28 19:03:11 -0700

    update function and pass test

--- updateSpots.js
+++ updateSpots.js
@@ -7,12 +7,47 @@
  * @return {Array}   A Days array we can save back into state.
  */
 
-const updateSpots = function(state, appointments, id) {
+const updateSpots = function (state, appointments, id) {
+  // make copy of days array
+  const days = state.days.map((day) => {
+    return { ...day };
+  });
 
-  // return an updated days array 
-  return [];
+  console.log("DAYS: ", days);
+
+  function findDay(day) {
+    const daysOfWeek = {
+      Monday: 0,
+      Tuesday: 1,
+      Wednesday: 2,
+      Thursday: 3,
+      Friday: 4,
+    };
+    return daysOfWeek[day];
+  }
+  // find the day's index in days array
+  const dayIndex = findDay(state.day);
+  console.log("STATE.DAY: ", state.day);
+  console.log("DAYINDEX: ", dayIndex);
+
+  // check for old state
+  const prevState = state.appointments[id].interview;
+  console.log("prevSTATE: ", prevState);
+  // check for new state
+  const newState = appointments[id].interview;
+  console.log("newSTATE: ", newState);
+
+  // create - no old state + new state
+  if (!prevState && newState) {
+    days[dayIndex].spots--;
+  }
+  // delete - old state + no new state
+  if (prevState && !newState) {
+    days[dayIndex].spots++;
+  }
+
+  // return days array
+  return days;
 };
 
 module.exports = updateSpots;
-
-
